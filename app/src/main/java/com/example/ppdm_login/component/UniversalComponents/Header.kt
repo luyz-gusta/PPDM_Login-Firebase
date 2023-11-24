f49a3c2dcad1aeebcd7c11334ff22e0869fdd56f
JOÃO VICTOR DA SILVA <22282229@dsn127m1246733.cfp127aluno.local>
2023-11-24 15:35:02 -0300

    fiz o laucher da imagem

--- app/src/main/java/com/example/ppdm_login/component/UniversalComponents/Header.kt
+++ app/src/main/java/com/example/ppdm_login/component/UniversalComponents/Header.kt
@@ -1,5 +1,9 @@
 package com.example.ppdm_login.component.UniversalComponents
 
+import android.net.Uri
+import android.util.Log
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
@@ -14,18 +18,27 @@ import androidx.compose.material.icons.filled.PhotoCamera
 import androidx.compose.material3.Card
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import coil.compose.AsyncImage
 import coil.compose.AsyncImagePainter.State.Empty.painter
+import coil.compose.rememberAsyncImagePainter
+import coil.request.ImageRequest
 import com.example.ppdm_login.R
+import com.example.ppdm_login.fuctions.ConvertFirebase
 
 
 @Preview(showSystemUi = true)
@@ -33,6 +46,31 @@ import com.example.ppdm_login.R
 fun Header() {
 
     val camera = Icons.Default.PhotoCamera
+
+
+    val context = LocalContext.current
+
+    var fotoUri by remember {
+        mutableStateOf<Uri?>(null)
+    }
+
+    val painter = rememberAsyncImagePainter(
+        ImageRequest.Builder(context).data(fotoUri).build()
+    )
+
+    var imagem by remember {
+        mutableStateOf("https://icones.pro/wp-content/uploads/2021/02/icone-utilisateur-gris.png")
+    }
+
+
+
+    val launcher = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.GetContent()
+    ) { uri ->
+        uri?.let {
+            fotoUri = it
+        }
+    }
 
     Column(
         modifier = Modifier
@@ -65,7 +103,11 @@ fun Header() {
             Card(
                 modifier = Modifier
                     .size(100.dp)
-                    .align(alignment = Alignment.Center),
+                    .align(alignment = Alignment.Center)
+                    .clickable {
+                        launcher.launch("image/*")
+                        imagem = ConvertFirebase(imagem = fotoUri!!, context = context)
+                    },
                 shape = CircleShape,
                 border = BorderStroke(
                     width = 2.dp,
@@ -75,15 +117,16 @@ fun Header() {
                         )
                 )
             ) {
-                Image(
-                    painter = painterResource(id = R.drawable.profile),
-                    contentDescription = null,
+
+                AsyncImage(
+                    model = imagem,
+                    contentDescription = "",
                     modifier = Modifier
                         .size(64.dp),
                     contentScale = ContentScale.None,
-
-                    )
+                )
             }
+            Log.e("morri", "Header: ${imagem}", )
             Image(
                 imageVector = camera,
                 contentDescription = null,
@@ -94,6 +137,7 @@ fun Header() {
 
                     },
             )
+            Log.e("Aquiiiii", "Header: ${imagem}", )
         }
 
     }
