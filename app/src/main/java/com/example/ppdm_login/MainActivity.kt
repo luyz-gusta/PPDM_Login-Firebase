43710960222f264f6fac6aea2b8923813de76c8d
LUIZ GUSTAVO DA SILVA <22282203@dsn127m1246732.cfp127aluno.local>
2023-11-24 15:09:38 -0300

    Main arrumada

--- app/src/main/java/com/example/ppdm_login/MainActivity.kt
+++ app/src/main/java/com/example/ppdm_login/MainActivity.kt
@@ -10,6 +10,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.ppdm_login.component.Screen.SymbianScreen
 import com.example.ppdm_login.ui.theme.PPDMLoginTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,25 +23,9 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    SymbianScreen()
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    PPDMLoginTheme {
-        Greeting("Android")
     }
 }
