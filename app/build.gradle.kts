e1f49eba16aa355a134c9c1acc8f8154b713e6bb
LUIZ GUSTAVO DA SILVA <22282203@dsn127m1246732.cfp127aluno.local>
2023-11-24 13:44:47 -0300

    foiii

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -66,4 +66,62 @@ dependencies {
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
+
+    //Modificar imagem
+    implementation("io.coil-kt:coil-compose:2.3.0")
+    implementation("io.coil-kt:coil:2.3.0")
+
+    // Para utilização da lazyColunm
+    implementation("androidx.compose.foundation:foundation:1.0.3")
+    implementation("androidx.activity:activity-compose:1.3.1")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:1.0.0-alpha07")
+
+    // Icons
+    implementation ("androidx.compose.material:material-icons-extended:1.2.0")
+
+    // Retrofit
+    implementation ("com.google.code.gson:gson:2.8.9")
+    implementation ("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation ("com.squareup.okhttp3:logging-interceptor:5.0.0-alpha.1")
+    implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.4")
+    implementation ("androidx.lifecycle:lifecycle-runtime-ktx:2.6.0")
+
+
+    //Implementacao do bottom navigation
+    implementation ("androidx.compose.material:material:1.4.2")
+
+    //importe da dependencia do navControler
+    implementation("androidx.navigation:navigation-compose:2.6.0")
+    implementation("com.google.accompanist:accompanist-navigation-animation:0.30.1")
+
+    implementation ("androidx.lifecycle:lifecycle-viewmodel:2.6.2")
+
+    // implementação de data no textfiild
+    implementation("androidx.compose.material3:material3:1.2.0-alpha02")
+    implementation("com.google.android.material:material:1.6.0-alpha01")
+
+    // teste
+    // Dependência para a biblioteca do Jetpack Compose
+    implementation("androidx.compose.ui:ui:1.1.0")
+
+    // Dependência para componentes de IU do Jetpack Compose
+    implementation("androidx.compose.foundation:foundation:1.1.0")
+    implementation("androidx.compose.runtime:runtime:1.1.0")
+
+    // Dependência para componentes de IU do Material Design no Jetpack Compose
+    implementation("androidx.compose.material:material:1.1.0")
+
+    // Dependência para componentes de IU do Jetpack Navigation Compose
+    implementation("androidx.navigation:navigation-compose:2.4.0")
+
+    // Dependência para ViewModel no Jetpack Compose
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:1.0.0-alpha07")
+
+    // Dependência para Hilt (para injeção de dependência no Jetpack Compose)
+    implementation("androidx.hilt:hilt-navigation-compose:1.0.0-alpha03")
+
+    // Dependência do compose material
+    implementation("androidx.compose.material3:material3:x.y.z")
+    implementation ("androidx.compose.material:material:1.4.2")
 }
